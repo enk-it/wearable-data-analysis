38c84e7ad8aa681b1fc9d6c04449d867c4c8bd03
enk-it <gladysh.danil@gmail.com>
2025-04-06 15:39:33 +0700

    feat: Add setup.sql for creating tables in clickhouse

---
+++ setup.sql
@@ -0,0 +1,41 @@
+CREATE TABLE sleep (
+    date DateTime,
+    deep_sleep_time UInt32,
+    shallow_sleep_time UInt32,
+    wake_time UInt32,
+    start DateTime,
+    stop DateTime,
+    rem_time UInt32,
+    naps String
+)
+ENGINE = MergeTree()
+ORDER BY (date);
+
+CREATE TABLE activity_stage (
+    date DateTime,
+    start_time DateTime,
+    stop_time DateTime,
+    distance UInt32,
+    calories UInt32,
+    steps UInt32
+)
+ENGINE = MergeTree()
+ORDER BY (date, start_time);
+
+CREATE TABLE activity_minute (
+    date DateTime,
+    time DateTime,
+    steps UInt32
+)
+ENGINE = MergeTree()
+ORDER BY (date, time);
+
+CREATE TABLE activity (
+    date DateTime,
+    steps UInt32,
+    distance UInt32,
+    run_distance UInt32,
+    calories UInt32
+)
+ENGINE = MergeTree()
+ORDER BY (date);
